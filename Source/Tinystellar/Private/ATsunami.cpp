87d33177eb99dd655182784fe5c74d38625f223d
brunogbrito <brunogbrito@gmail.com>
2019-11-29 16:04:06 +0100

    Fixed/Prepared Tsunami shaders

---
+++ Source/Tinystellar/Private/ATsunami.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ATsunami.h"
+
+// Sets default values
+AATsunami::AATsunami()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AATsunami::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AATsunami::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
